2e8b1bc47bfa148feb90d91b268ebec502aff5e9
Gokul <gokull.mani@outlook.com>
2021-03-01 01:19:32 -0500

    Comments for functions

--- test/ReferFiles/reduxApp.js
+++ test/ReferFiles/reduxApp.js
@@ -5,6 +5,13 @@ const or=require('../objectRepository')
 
     module.exports={
         post:{
+            /**
+             * Creates a Post with the given title, authorName, and content
+             * @page pass page to playwright
+             * @title the title of the post
+             * @authorName the author name of the post
+             * @content the content for the post
+             */             
             async createPost(page,title,authorName,content){
                 await page.type('#postTitle',title)
                 const author = await page.$("#postAuthor");
@@ -14,6 +21,12 @@ const or=require('../objectRepository')
                 await page.click('form>button')
                 await page.waitFor(3000)
             },
+            /**
+             * Edits a Post with the given title and content
+             * @page pass page to playwright
+             * @title the title of the post that is to be edited
+             * @content the content for the post that is to be edited
+             */ 
             async editPost(page,title,content){
                 await page.click(or.post.editPost)
                 await page.click(or.post.title,{clickCount: 3})
@@ -32,7 +45,7 @@ const or=require('../objectRepository')
 
         },
         common:{
-            
+
         }
         
     }
